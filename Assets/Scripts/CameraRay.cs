570e188d6d995caa8232f929c0d2f1ded7a65d26
Viktoria-1 <e1526186@student.tuwien.ac.at>
2022-03-13 18:45:25 +0100

    Add editor like camera movement
    Add editor like camera movement

--- Assets/Scripts/CameraRay.cs
+++ Assets/Scripts/CameraRay.cs
@@ -8,6 +8,17 @@ public class CameraRay : MonoBehaviour
 {
 	private bool movementLeft = true;
 	
+	[SerializeField] float navigationSpeed = 2.4f;
+	[SerializeField] float shiftMultiplier = 2f;
+	[SerializeField] float sensitivity = 1.0f;
+ 
+	private Camera cam;
+	private Vector3 anchorPoint;
+	private Quaternion anchorRot;
+	
+	private void Awake () {
+		cam = GetComponent<Camera>();
+	}
     // Start is called before the first frame update
     void Start()
     {
@@ -38,5 +49,39 @@ public class CameraRay : MonoBehaviour
 			
 		}
         
+		//can be commented out/removed
+		//as long as you hold the right mouse button, you can navigate through the scene
+		if(Input.GetMouseButton(1)) {
+			Vector3 move = Vector3.zero;
+			float speed = navigationSpeed * (Input.GetKey(KeyCode.LeftShift) ? shiftMultiplier : 1f) * Time.deltaTime * 9.1f;
+			if(Input.GetKey(KeyCode.W))
+				move += Vector3.forward * speed;
+			if(Input.GetKey(KeyCode.S))
+				move -= Vector3.forward * speed;
+			if(Input.GetKey(KeyCode.F))
+				move += Vector3.right * speed;
+			if(Input.GetKey(KeyCode.A))
+				move -= Vector3.right * speed;
+			if(Input.GetKey(KeyCode.E))
+				move += Vector3.up * speed;
+			if(Input.GetKey(KeyCode.Q))
+				move -= Vector3.up * speed;
+			transform.Translate(move);
+		}
+ 
+		if(Input.GetMouseButtonDown(1)) {
+			anchorPoint = new Vector3(Input.mousePosition.y, -Input.mousePosition.x);
+			anchorRot = transform.rotation;
+		}
+		if(Input.GetMouseButton(1)) {
+			Quaternion rot = anchorRot;
+ 
+			Vector3 dif = anchorPoint - new Vector3(Input.mousePosition.y, -Input.mousePosition.x);
+			rot.eulerAngles += dif * sensitivity;
+			transform.rotation = rot;
+		}
+		
+		//Zoom in and out with Mouse Wheel
+		this.transform.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel") * this.navigationSpeed, Space.Self);
     }
 }
